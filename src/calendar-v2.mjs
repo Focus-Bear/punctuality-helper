0fc0073fbe74e97b6899c53575d4dbeeb1022402
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2022-12-13 10:30:49 -0500

    applescript wrappers working

--- src/calendar-v2.mjs
+++ src/calendar-v2.mjs
@@ -9,12 +9,13 @@ const header = `
 		set theStore to fetch store
 	 `;
 
-
 async function exec(script) {
   return new Promise((resolve, reject) => {
     return applescript.execString(header + script, (err, rtn) => {
       if (err) {
         // Something went wrong!
+        console.log('Error:', err);
+        reject(err);
       }
       resolve(rtn);
     });
@@ -23,24 +24,21 @@ async function exec(script) {
 
 async function getCalendars() {
   const script = `
-	   set theCals to fetch calendars {} event store theStore -- change to suit
+	    set theCals to fetch calendars {} event store theStore -- change to suit
 		set theTitles to {}
 		repeat with aCal in theCals
-		set calTitle to aCal's title() as text
-		log calTitle
-		--set end of theTitles to aCal's title() as text
-		 set theTitles to theTitles & calTitle   
+	      set calTitle to aCal's title() as text
+		  log calTitle
+		  set theTitles to theTitles & calTitle   
 		end repeat
 
-		return theTitles
-				`;
+		return theTitles`;
 
-   return 	await exec(script)
+  return await exec(script);
 }
 
-async function getEvents(){
- const script = `
-
+async function getEvents() {
+  const script = `
     set theCals to fetch calendars {} event store theStore
 
    	set d1 to (current date)
@@ -60,17 +58,30 @@ async function getEvents(){
 		    copy event_description of (event info for event anEvent) as text to end of current
 			copy event_external_ID of (event info for event anEvent) as text to end of current
 	   	end try
-		set output to output & current
+		copy current to end of output 
     end repeat 
     return output
 `;
-			return await exec(script)
+  const rawEvents = await exec(script);
+  return rawEvents.filter(e => e.length).map(evt => {
+    const [summary, start_date, end_date, url, location, description, id] = evt;
+
+    return {
+      summary,
+      start_date,
+      end_date,
+      url,
+      location,
+      description,
+      id,
+    };
+  });
 }
+
 async function main() {
   const cals = await getCalendars();
-  console.log(cals);
-		const events = await getEvents();
-		console.log(events)
+  const events = await getEvents();
+  console.log(cals, events);
 }
 
 main().then(() => console.log('done'));
