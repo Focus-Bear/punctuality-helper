fcacee664a07af78ede1bb5a39b783279e8b2ca1
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2022-12-25 14:24:32 -0500

    disabled keyboard module

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-require("./src/keyboard.js");
+//require("./src/keyboard.js");
 
-const addTestEvents = require("./src/testing/index.js"),
+const addTestEvents = require('./src/testing/index.js'),
   {
     syncCalendarsToUpcoming,
     checkUpcomingForMeetings,
-  } = require("./src/upcoming.js");
+  } = require('./src/upcoming.js');
 
 const {
   SLOW_NAP_DURATION_MINUTES,
   QUICK_NAP_DURATION_SECONDS,
   IS_TESTING,
-} = require("./config.js");
+} = require('./config.js');
 
 const quickInterval = QUICK_NAP_DURATION_SECONDS * 1000,
   slowInterval = SLOW_NAP_DURATION_MINUTES * 60_000;
 
 async function main() {
-  console.log("Late No More Online..");
+  console.log('Late No More Online..');
 
   if (IS_TESTING) await addTestEvents();
   else syncCalendarsToUpcoming();
@@ -28,4 +28,4 @@ async function main() {
 
 main()
   .then(() => {})
-  .catch((e) => console.log(e));
+  .catch(e => console.log(e));
