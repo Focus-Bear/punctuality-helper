8f65429fdad00097f2bb1a267fd3b9799e927378
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-01-03 15:45:08 -0500

    changed local file ref -- not yet working

--- src/applescript/dialog.js
+++ src/applescript/dialog.js
@@ -1,29 +1,20 @@
-const exec = require('./exec.js');
-
-const files = require('./files.js');
-
-const url =
-  'https://user-images.githubusercontent.com/1034890/209779279-7b23e061-c5af-4129-9d51-1e674a0818e8.jpg';
+const exec = require("./exec.js");
 
 async function showDialog(
   title,
   text,
   buttons,
   givingUpAfter = 30,
-  showIcon = false,
+  showIcon = false
 ) {
-
-  const path = files(url)
   const SCRIPT = ` 
---set theImage to alias of thePath as string
-set theImage to (path to temporary items as text) & "focusbear.png"
-log theImage
-set dialogText to "${text}"
+set theImage to (POSIX file "/Users/Library/Application Support/com.focusbear.latenomore/icon.png") as text
+set dialogText to "${text}"                                                                                
 set button1Label to "${buttons[0]}"
 set button2Label to "${buttons[1]}"
 set timeoutValue to ${givingUpAfter} 
 set result to (display dialog dialogText with title "${title}" buttons {button1Label, button2Label} default button button1Label giving up after ${givingUpAfter} ${
-    showIcon ? 'with icon theImage' : ''
+    showIcon ? "with icon theImage" : ""
   })
 set buttonReturned to result's button returned
 return buttonReturned`;
@@ -39,4 +30,4 @@ set userInput to result's text returned
 return userInput`;
   return await exec(SCRIPT);
 }
-module.exports = {showDialog, askQuestion};
+module.exports = { showDialog, askQuestion };
