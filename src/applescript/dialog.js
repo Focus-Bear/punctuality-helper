38d3c79c426d28b7c329dcf64148aed7be04cc93
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-01-03 17:02:27 -0500

    disabled icon

--- src/applescript/dialog.js
+++ src/applescript/dialog.js
@@ -9,13 +9,13 @@ async function showDialog(
 ) {
     showIcon = false
     const SCRIPT = ` 
-set theImage to alias ((path to library folder from system domain as text ) & "com.focusbear.latenomore:logo.png") 
+set theImage to ((path to library folder from system domain ) & "com.focusbear.latenomore:logo.png") as string 
 set dialogText to "${text}"                                                                                
 set button1Label to "${buttons[0]}"
 set button2Label to "${buttons[1]}"
 set timeoutValue to ${givingUpAfter} 
 set result to (display dialog dialogText with title "${title}" buttons {button1Label, button2Label} default button button1Label giving up after ${givingUpAfter} ${
-        showIcon ? 'with icon file theImage' : ''
+        showIcon ? 'with icon posix file theImage' : ''
     })
 set buttonReturned to result's button returned
 return buttonReturned`
